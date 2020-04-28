990c77ddd955ca5d70e02080b42cbf670716cc0a
mcshakes <efmccarthy26@gmail.com>
2020-04-27 21:40:58 -0600

    Connected to the guild

--- gucci.py
+++ gucci.py
@@ -3,3 +3,21 @@ import discord
 from dotenv import load_dotenv
 load_dotenv()
 
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('TEST_GUILD')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+client.run(TOKEN)
